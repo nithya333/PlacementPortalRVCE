9a7ed638daa4e53b5862d26a46b71ead78947440
M Nithyashree <121751642+nithya333@users.noreply.github.com>
2025-03-24 20:08:00 +0530

    Update db.py

--- db.py
+++ db.py
@@ -8,9 +8,8 @@ class MongoDBClient:
     @staticmethod
     def get_client():
         if MongoDBClient._client is None:
-            # Initialize MongoDB client if not created already
-            # uri = os.getenv('MONGO_URI', 'mongodb+srv://?retryWrites=true&w=majority&appName=Cluster0Charitham')  # Use your MongoDB URI here
-            uri = 'mongodb+srv://nithya3169:MTUsn5fNh1xOurY5@cluster0charitham.hdany.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0Charitham'  
+            # Initialize MongoDB client if not created already 
+            uri = 'mongodb+srv://?retryWrites=true&w=majority&appName=Cluster0Charitham'  # Use your MongoDB URI here
             
             MongoDBClient._client = MongoClient(uri)
         return MongoDBClient._client
